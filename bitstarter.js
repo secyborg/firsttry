5276d41764901675a9869e2c90202f99df5430d7
Cyborg Se <cyborg@gmail.com>
2014-03-21 02:18:30 +0000

    read from index

--- bitstarter.js
+++ bitstarter.js
@@ -3,7 +3,8 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World! Message New!!');
+  var index = fs.readFileSync("index.html"); 
+  response.send(index.toString());
 });
 
 var port = process.env.PORT || 5000;
